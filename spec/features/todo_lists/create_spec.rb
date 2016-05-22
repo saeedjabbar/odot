4f81ac152c35f023ce14cfe0d9d26aaf3d21c467
Saeed Jabbar <saeedryanjabbar@gmail.com>
2016-05-22 01:49:26 -0400

    more tests

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -47,6 +47,7 @@ describe "Creating Todo lists" do
     visit "/todo_lists"
     expect(page).to_not have_content("This is what I'm doing today.")
   end
+ 
   it "displays an error when the todo list has no description" do
     expect(TodoList.count).to eq(0)
     visit "/todo_lists"
@@ -55,6 +56,23 @@ describe "Creating Todo lists" do
 
     fill_in "Title", with: "Grocery List"
     fill_in "Description", with: ""
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("Grocery List")
+  end
+  
+  it "displays an error when the todo list has description under 4 chars" do
+    expect(TodoList.count).to eq(0)
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New todo_list")
+
+    fill_in "Title", with: "Grocery List"
+    fill_in "Description", with: "Food"
     click_button "Create Todo list"
 
     expect(page).to have_content("error")
